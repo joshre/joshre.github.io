b0fa02b8bb195bebeefffab90e44d197c775a9b2
Josh Reeder-Esparza <joshre@me.com>
2024-10-24 12:05:56 -0400

    Updated Nav

--- js/site.js
+++ js/site.js
@@ -39,15 +39,28 @@ mobileNavHandler = function () {
   let body = document.querySelector("body");
   let nav = document.querySelector(".nav");
   let links = Array.from(nav.querySelectorAll("a"));
+  
   links.push(button);
 
-  links.forEach((el) => {
-    handleTouchClick(el, () => {
-      body.classList.toggle("navopen");
-      let isNavOpen = body.classList.contains("navopen");
+  const toggleNav = (el) => {
+    body.classList.toggle("navopen");
+    let isNavOpen = body.classList.contains("navopen");
 
-      el.setAttribute("aria-expanded", isNavOpen);
-      nav.setAttribute("aria-hidden", !isNavOpen);
+    button.setAttribute("aria-expanded", isNavOpen);
+    nav.setAttribute("aria-hidden", !isNavOpen);
+
+    if (isNavOpen) {
+      nav.classList.remove("pointer-events-none");
+      nav.classList.add("pointer-events-auto");
+    } else {
+      nav.classList.remove("pointer-events-auto");
+      nav.classList.add("pointer-events-none");
+    }
+  };
+
+  links.forEach((el) => {
+    el.addEventListener("click", () => {
+      toggleNav(el);
     });
   });
 };
